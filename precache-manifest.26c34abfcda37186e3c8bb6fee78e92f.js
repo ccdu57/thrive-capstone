8c3707a599a2df40dfdaa01f0fea5f49d25ff5f1
ccdu57 <chris.caicedo@gmail.com>
2019-11-26 10:09:07 -0500

    Updates

---
+++ precache-manifest.26c34abfcda37186e3c8bb6fee78e92f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c4ede59881d231a8fcaa1cd67ee1ef",
+    "url": "/thrive-capstone/index.html"
+  },
+  {
+    "revision": "b21de88d42fb3a66d10d",
+    "url": "/thrive-capstone/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "4020ea71c6799617162a",
+    "url": "/thrive-capstone/static/js/2.842ce10d.chunk.js"
+  },
+  {
+    "revision": "b21de88d42fb3a66d10d",
+    "url": "/thrive-capstone/static/js/main.195b50ab.chunk.js"
+  },
+  {
+    "revision": "4039ee4a1cd5e96a5a3b",
+    "url": "/thrive-capstone/static/js/runtime-main.0a9b578a.js"
+  }
+]);
